e58c329e4c3f76e77399358d3b61117ecd27f954
Rajesh <168886352+rajeshgithup@users.noreply.github.com>
2024-07-26 16:28:23 +0530

    Update Controll.java

--- src/main/java/com/example/demo/Controll.java
+++ src/main/java/com/example/demo/Controll.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controll {
 @GetMapping("/get")
 public String get() {
-	return "Docker Run";
+	return "New Docker  Run";
 }
 }
